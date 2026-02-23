7064f0fea05988c3cbc67da04bdcf85c3a06b558
mayoite <mayoite@gmail.com>
2026-02-24 00:49:33 +0530

    Stop warning about missing NEXT_PUBLIC_SUPABASE_URL

--- lib/db.ts
+++ lib/db.ts
@@ -3,8 +3,5 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://erpweaiypimorcunaimz.supabase.co";
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "sb_publishable_21J6H7rVe81Ray_WLUDgEA_WU9iTNsG";
 
-if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
-    console.warn("⚠️ Missing NEXT_PUBLIC_SUPABASE_URL. Using fallback value.");
-}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
